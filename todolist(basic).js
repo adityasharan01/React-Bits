1675367d0944f13dd7cb707b42345ea1fab1f3c2
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-02-23 01:07:24 +0530

    Update todolist(basic).js

--- todolist(basic).js
+++ todolist(basic).js
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 
 const App = () => { 
   const bioData = [{ id:1,name: 'aditya', age: 23 }, { id:2,name: 'harsh', age: 22 }, { id:3,name: 'Amit', age: 21 }, { id:4,name: 'saket', age: 20 }];
-  const [myArray, setMyArray] = useState(bioData);
+  const [myArray,setMyArray] = useState(bioData);
   const clearArray = () => {
+    
     setMyArray([]);
   }
   const clearElem = (id) => {

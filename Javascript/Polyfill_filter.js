028bf87068e350bf3e908830a5798af35bfa99dc
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-07-22 00:20:17 +0530

    Create Polyfill_filter.js

---
+++ Javascript/Polyfill_filter.js
@@ -0,0 +1,20 @@
+Array.prototype.filterAlbums = function(callback, context) {
+  arr = []
+  for (var i = 0; i < this.length; i++) {
+    if (callback.call(context, this[i], i, this)) {
+      arr.push(this[i])
+    }
+  }
+  return arr
+}
+logicAlbums.filterAlbums(function(album) {
+  return album.rating > 4.9 // providing the context here
+})
+console.log(logicAlbums)
+
+// [
+//   { name: 'Bobby Tarantino', rating: 5 },
+//   { name: 'The Incredible True Story', rating: 4.5 },
+//   { name: 'Supermarket', rating: 4.9 },
+//   { name: 'Under Pressure', rating: 5 }
+// ];

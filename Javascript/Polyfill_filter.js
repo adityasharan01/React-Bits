517b49409070ea9e95349a781bc56ab7d0ec91ef
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-10-08 01:38:10 +0530

    Update Polyfill_filter.js

--- Javascript/Polyfill_filter.js
+++ Javascript/Polyfill_filter.js
@@ -11,7 +11,7 @@ logicAlbums.filterAlbums(function(album) {
   return album.rating > 4.9 // providing the context here
 })
 console.log(logicAlbums)
-
+// Need to do thsi
 // [
 //   { name: 'Bobby Tarantino', rating: 5 },
 //   { name: 'The Incredible True Story', rating: 4.5 },
